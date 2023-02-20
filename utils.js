9d46a293c77500524bff9c95755bbb92b4753d0d
ibnsamy96 <mahmoudsamy50@outlook.com>
2023-02-20 08:42:12 +0200

    Add note and cancel prompt utilities

--- utils.js
+++ utils.js
@@ -1,8 +1,12 @@
-import { isCancel, cancel } from "@clack/prompts";
+import { isCancel, cancel, note } from "@clack/prompts";
 
 export const addCancelPrompt = (inputField, cancelationMessage) => {
 	if (isCancel(inputField)) {
 		cancel(cancelationMessage);
 		process.exit(0);
 	}
+};
+
+export const addNote = (title, message) => {
+	note(message, title);
 };
