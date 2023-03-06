ef15fc0c833072877752d5aff36ec9060c9198b0
ibnsamy96 <mahmoudsamy50@outlook.com>
2023-03-06 16:47:18 +0200

    Update folders location

---
+++ bin/lbx-downloader.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+import { platform } from "os";
+import { spawn } from "child_process";
+import startApp from "../src/index.js";
+
+const args = process.argv.slice(2);
+
+const scriptIndex = args.findIndex((x) => x === "dir");
+const script = scriptIndex === -1 ? args[0] : args[scriptIndex];
+
+// console.log(args);
+
+switch (script) {
+	case "dir":
+		const path = "./";
+		let explorer;
+		switch (platform()) {
+			case "win32":
+				explorer = "explorer";
+				break;
+			case "linux":
+				explorer = "xdg-open";
+				break;
+			case "darwin":
+				explorer = "open";
+				break;
+		}
+		spawn(explorer, [path], { detached: true }).unref();
+		break;
+
+	default:
+		startApp();
+		break;
+}
