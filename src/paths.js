a013098c2d019fdf7df0a6184c480b5e3539ed8f
ibnsamy96 <mahmoudsamy50@outlook.com>
2023-07-06 03:51:09 +0300

    updated paths to add configs

--- src/paths.js
+++ src/paths.js
@@ -1,14 +1,24 @@
-import { createDirIfNotExist } from "./helpers.js"
+import {
+	createDirIfNotExist,
+	createFileIfNotExist,
+	generateFilePath,
+	parseConfigsFile,
+} from "./helpers.js"
 import path from "path"
 import { homedir } from "os"
+import fs from "fs"
 
 const base = path.join(homedir(), "linkbox-downloader")
 createDirIfNotExist(base)
 
-const downloads = path.join(base, "downloads")
-createDirIfNotExist(downloads)
-
 const logs = path.join(base, "linkbox_logs")
 createDirIfNotExist(logs)
 
-export default { base, downloads, logs }
+const configs = generateFilePath([base, ".configs"])
+createFileIfNotExist(configs)
+
+const downPathConfig = parseConfigsFile({ filePath: configs })["download-dir"]
+const downloads = downPathConfig || path.join(base, "downloads")
+createDirIfNotExist(downloads)
+
+export default { base, downloads, logs, configs }
